05a39b7efaa10e95d321d11c898e643a6c4a92cf
Wild-Hound <wildhound404@gmail.com>
2024-05-12 02:11:01 +0600

    code refactored

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// const canvas = document.getElementById("game");
 const THREE = window.THREE;
-const scene = new THREE.Scene();
 
 class Cube {
   constructor(x, y, z, color, scene) {
@@ -12,36 +10,65 @@ class Cube {
   }
 }
 
-//setup scene
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
-scene.add(ambientLight);
+const initGame = () => {
+  const scene = new THREE.Scene();
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
-directionalLight.position.set(10, 20, 0);
-scene.add(directionalLight);
+  //setup scene
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+  scene.add(ambientLight);
 
-const cube1 = new Cube(0, 0, 0, 0xfb8e00, scene);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+  directionalLight.position.set(10, 20, 0);
+  scene.add(directionalLight);
 
-//setup camera
-const aspect = window.innerWidth / window.innerHeight;
-const width = 10;
-const height = width / aspect;
+  new Cube(0, -1, 0, 0xfb8e00, scene);
 
-const camera = new THREE.OrthographicCamera(
-  width / -1,
-  width,
-  height,
-  height / -1,
-  1,
-  100
-);
+  //setup camera
+  const aspect = window.innerWidth / window.innerHeight;
+  const width = 10;
+  const height = width / aspect;
 
-camera.position.set(4, 4, 4);
-camera.lookAt(0, 0, 0);
+  const camera = new THREE.OrthographicCamera(
+    width / -1,
+    width,
+    height,
+    height / -1,
+    1,
+    100
+  );
+  camera.position.set(4, 4, 4);
+  camera.lookAt(0, 0, 0);
 
-//render game
-const renderer = new THREE.WebGL1Renderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
+  //render game
+  const renderer = new THREE.WebGL1Renderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  return { scene, camera, renderer };
+};
+
+const addLayer = (x, z, width, depth, direction, boxHeight, stack, scene) => {
+  console.log("yoyo");
+  const y = boxHeight * stack.length;
+  const color = `hsl(${30 + stack.length * 4}, 100%, 50%)`;
+
+  const cube = new Cube(x, y, z, color, scene);
+
+  const cubeObj = { threejs: cube, width, depth, direction };
+  stack.push(cubeObj);
+};
+
+const addLayerAdapter = (stack, scene, boxHeight) => {
+  return {
+    addLayer: (x, z, width, depth, direction) =>
+      addLayer(x, z, width, depth, direction, boxHeight, stack, scene),
+  };
+};
+
+const { scene, camera, renderer } = initGame();
+const gameStack = [];
+
+const { addLayer: addLayerTemp } = addLayerAdapter(gameStack, scene, 1);
+addLayerTemp(0, 0, 1, 1, "x");
+
 renderer.render(scene, camera);
-
 document.body.appendChild(renderer.domElement);
